b6fdfacdc83a6fe11f2933e8b2a053a9484f3add
adetonn <90422155+adetonn@users.noreply.github.com>
2021-11-28 18:30:47 -0500

    Create gameCode.js

---
+++ gameCode.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//test
+
+fill(0,0,0);
+rect(100,100,100,100); 
+text("hello", 100, 200); 
